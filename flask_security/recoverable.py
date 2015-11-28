d3bc386bc6a3510ca5ae250d502c742aaeed1087
Charlie Wolf <charlie@wolf.is>
2015-11-28 12:05:55 -0800

    unjustify

--- flask_security/recoverable.py
+++ flask_security/recoverable.py
@@ -5,14 +5,15 @@
 
     Flask-Security recoverable module
 
-    :copyright: (c) 2012 by Matt Wright. license: MIT, see LICENSE for 
-    :more details.
+    :copyright: (c) 2012 by Matt Wright.
+    :license: MIT, see LICENSE for more details.
 """
 
-from flask import current_app as app from werkzeug.local import 
-LocalProxy from werkzeug.security import safe_str_cmp
+from flask import current_app as app
+from werkzeug.local import LocalProxy
+from werkzeug.security import safe_str_cmp
 
-from .signals import password_reset, reset_password_instructions_sent 
+from .signals import password_reset, reset_password_instructions_sent
 from .utils import send_mail, encrypt_password, url_for_security, \
     get_token_status, config_value
 
@@ -24,21 +25,18 @@ _datastore = LocalProxy(lambda: _security.datastore)
 
 
 def send_reset_password_instructions(user):
-    """Sends the reset password instructions email for the specified 
-user.
+    """Sends the reset password instructions email for the specified user.
 
     :param user: The user to send the instructions to
     """
     token = generate_reset_password_token(user)
-    reset_link = url_for_security('reset_password', token=token, 
-_external=True)
+    reset_link = url_for_security('reset_password', token=token, _external=True)
 
     send_mail(config_value('EMAIL_SUBJECT_PASSWORD_RESET'), user.email,
               'reset_instructions',
               user=user, reset_link=reset_link)
 
-    reset_password_instructions_sent.send(app._get_current_object(), 
-user=user, token=token)
+    reset_password_instructions_sent.send(app._get_current_object(), user=user, token=token)
 
 
 def send_password_reset_notice(user):
@@ -47,8 +45,7 @@ def send_password_reset_notice(user):
     :param user: The user to send the notice to
     """
     if config_value('SEND_PASSWORD_RESET_NOTICE_EMAIL'):
-        send_mail(config_value('EMAIL_SUBJECT_PASSWORD_NOTICE'), 
-user.email,
+        send_mail(config_value('EMAIL_SUBJECT_PASSWORD_NOTICE'), user.email,
                   'reset_notice', user=user)
 
 
@@ -63,17 +60,14 @@ def generate_reset_password_token(user):
 
 
 def reset_password_token_status(token):
-    """Returns the expired status, invalid status, and user of a 
-password reset
+    """Returns the expired status, invalid status, and user of a password reset
     token. For example::
 
-        expired, invalid, user, data = 
-reset_password_token_status('...')
+        expired, invalid, user, data = reset_password_token_status('...')
 
     :param token: The password reset token
     """
-    expired, invalid, user, data = get_token_status(token, 'reset', 
-'RESET_PASSWORD',
+    expired, invalid, user, data = get_token_status(token, 'reset', 'RESET_PASSWORD',
                                                     return_data=True)
     if not invalid:
         if user.password:
@@ -87,9 +81,11 @@ reset_password_token_status('...')
 def update_password(user, password):
     """Update the specified user's password
 
-    :param user: The user to update_password param password: The 
-    :unencrypted new password
+    :param user: The user to update_password
+    :param password: The unencrypted new password
     """
+    if not config_value('DISABLE_ENCRYPTION'):
+        user.password = encrypt_password(password)
     _datastore.put(user)
     send_password_reset_notice(user)
     password_reset.send(app._get_current_object(), user=user)
